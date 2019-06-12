696caa8ac5618d8fbe08e8f97e67ccdbc86392b9
Aidanchase <aidanmro@gmail.com>
2019-06-12 15:49:30 +0000

    added color status to counter

--- public/scripts/composer-num-counter.js
+++ public/scripts/composer-num-counter.js
@@ -3,8 +3,18 @@ $(document).ready(function() {
         //find input lengtt
     let tweetLength = $(this).val().length;
     let remainingLength = 140 - tweetLength;
+    //find siblings at class counter, reassign its text to new length
     $(this).siblings(".counter").text(remainingLength);
+    let status;
+     if (remainingLength < 0){
+         status = "over";
+     } else if (remainingLength <= 10){
+         status = "warning";
+     } else {
+         status = "normal";
+     }
+     $(this).siblings(".counter").attr("data-status", status);
+    });
+    });
 
-    })
-});
   
