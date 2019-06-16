fad83e811e56c5ca3f3e479bb103ddd0eb7a9eb5
Aidanchase <aidanmro@gmail.com>
2019-06-16 22:11:23 +0000

    fixed time calc again

--- public/scripts/tweet-age-calc.js
+++ public/scripts/tweet-age-calc.js
@@ -1,48 +1,47 @@
 function timeAgo(ts) {
-   //calculating time since post
-   const d = new Date();
-   const nowTs = d.getTime();
-   //calculate difference of milliseconds
-   const seconds = Math.floor((nowTs - ts) / 1000);
-   
-   if (seconds > 365 * 24 * 3600) { //if more than a year 
+                     //calculates  time in seconds
+   var date = new Date();
+   var nowTime = date.getTime();
+                   //get milliseconds and / by 1000 for secs
+   var seconds = Math.floor((nowTime - ts) / 1000);
+
+   // over a year
+   if(seconds > 365 * 24* 3600){
       return "Over a year ago";
-   } else if (seconds > 2 * 24 * 3600) { //if more than 2 days
+   }
+
+   // more than two days
+   else if (seconds > 2 * 24 * 3600) {
       return Math.floor(seconds / 60 / 60 / 24) + " days ago";
    }
-   //if more than a day
+   // a day
    else if (seconds > 24 * 3600) {
       return "Yesterday";
    }
-   //time in hours
+   // Hours
    else if (seconds > 3600) {
       const hours = Math.floor(seconds / 60 / 60).toString();
       const hour = (hours > 1 ? "hours" : "hour").toString();
       return hours + " " + hour + " ago";
    }
-   // if within half Hour
+   // Half Hour
    else if (seconds > 1800) {
       return "Half an hour ago";
    }
-
-   else if (seconds > 120){
-      return "Over 2 minutes ago"
-   }
-   //if within Minutes
+   // Minutes
    else if (seconds > 60) {
-    return "Over a minute ago";
+      return Math.floor(seconds / 60) + " minutes ago";
    }
 
-   // if within a minute
-   else if (seconds > 10) {
+   // Within a minute
+   else if (seconds > 10){
       return seconds + " seconds ago";
- 
-   } else if (seconds > -1) {
+   }
+   // Just now
+   else if (seconds > -1){
       return "Just a moment ago"
-   
    } else {
-      // default
-      return "Unable to calculate age"
-
+   // Default condition
+   return "Unable to calculate age"
    }
 }
